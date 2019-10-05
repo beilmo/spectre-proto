95857ac41c856c5baa52566ebdc43998d3f9c721
Dorin Danciu <1619134+dorindanciu@users.noreply.github.com>
2019-10-05 17:34:44 +0300

    Store generated sources into Out directory

--- make.sh
+++ make.sh
@@ -1,12 +1,15 @@
+mkdir -p Out/Swift
 protoc \
     --swift_opt=Visibility=Public \
-    --swift_out="./Swift" \
+    --swift_out="./Out/Swift" \
     *.proto
 
+mkdir -p Out/Go
 protoc \
-    --go_out=./Go \
+    --go_out=./Out/Go \
     *.proto
 
+mkdir -p Out/Java
 protoc \
-    --java_out="./Java" \
+    --java_out="./Out/Java" \
     *.proto
